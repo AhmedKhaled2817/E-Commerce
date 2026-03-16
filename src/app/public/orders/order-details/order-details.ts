f86014dcf4922910d0d943209c143e889c0136cd
Ahmed Khaled <akhaledafify@gmail.com>
2026-03-16 22:06:53 +0200

    feat(order-details): enhance order tracking UI with animated progress line
    - Added dynamic progress line width based on current order step
    - Highlight active step and completed steps with colors and check icons
    - Display order date on current step
    - Fixed cancelled order progress calculation
    - Improved SCSS nesting for .order-tracking and .step
    - Corrected component decorator typo (styleUrls instead of styleUrl

--- src/app/public/orders/order-details/order-details.ts
+++ src/app/public/orders/order-details/order-details.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Order } from 'app/Shared/Models/order';
+import { Order, orderStatus } from 'app/Shared/Models/order';
 import { OrderService } from 'app/Shared/Service/order-service';
 
 @Component({
@@ -18,14 +18,51 @@ export class OrderDetails  implements OnInit {
   private orderService=inject(OrderService);
 
   order!:Order;
+
+  currentStep = 0;
+  progressWidth = '0%';
+
   ngOnInit(): void {
       const id = Number(this.ActivatedRoute.snapshot.paramMap.get('id'));
     this.orderService.orders$.subscribe((o)=>{
      const found=o.find((order)=>order.id===id)
      if(found){
       this.order=found;
+      this.currentStep=this.calculateCurrentStep();
+      this.progressWidth=this.calculateProgressWidth();
      }
     })
   }
+
+  steps=[
+    "Order Placed",
+    "Processing",
+    "Shipped",
+    "Delivered"
+  ]
+
+  calculateCurrentStep():number{
+    if(!this.order.status) return 0;
+    switch (this.order.status){
+      case orderStatus.Pending:
+          return 0;
+        case orderStatus.Shipped:
+          return 2;
+        case orderStatus.Delivered:
+          return 3
+        case orderStatus.Cancelled:
+          return -1
+          default :
+          return 0;
+    }
+  }
+
+  calculateProgressWidth(): string {
+
+  const progress = ((this.currentStep + 1) / this.steps.length) * 100;
+
+  return progress + '%';
+
+}
 
 }
