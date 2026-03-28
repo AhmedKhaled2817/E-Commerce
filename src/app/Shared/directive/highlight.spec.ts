908e391e5d881c4f61df2c538b0546f78e48d993
Ahmed Khaled <akhaledafify@gmail.com>
2026-03-28 19:01:10 +0200

    updated fixed all problem

--- src/app/Shared/directive/highlight.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef} from '@angular/core';
-import { Highlight } from './highlight';
-
-describe('Highlight', () => {
-  it('should create an instance', () => {
-    const directive = new Highlight(new ElementRef(null), null as any);
-    expect(directive).toBeTruthy();
-  });
-});
